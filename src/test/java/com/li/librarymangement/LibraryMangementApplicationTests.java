6630de680d858386d5763b4e474727e9e7afaae0
HL-Light <li2053660167@gmail.com>
2023-04-19 16:52:48 +0800

    first commit

---
+++ src/test/java/com/li/librarymangement/LibraryMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.librarymangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
